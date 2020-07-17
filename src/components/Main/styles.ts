dd2d683ab06ae52b83859ea918aad47b7c2099a0
mateusamarall <mateus.amaral018@gmail.com>
2020-07-17 20:44:04 -0300

    new style

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,5 +1,11 @@
-import styled from 'styled-components';
-import { ArrowLeft } from '../../styles/icons';
+import styled, { css } from 'styled-components';
+import {
+  ArrowLeft,
+  Home,
+  Search,
+  Notifications,
+  Email,
+} from '../../styles/icons';
 
 export const Container = styled.div`
   display: flex;
@@ -46,4 +52,65 @@ export const BackIcon = styled(ArrowLeft)`
   fill: var(--twitter);
 `;
 
-export const Profileinfo = styled.div``;
+export const Profileinfo = styled.div`
+  margin-left: 17px;
+
+  display: flex;
+  flex-direction: column;
+
+  > strong {
+    font-size: 19px;
+  }
+
+  > span {
+    font-size: 15px;
+
+    color: var(--gray);
+  }
+`;
+export const BottomMenu = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 2;
+
+  background: var(--primary);
+  width: 100%;
+  border-top: 1px solid var(--outline);
+
+  display: flex;
+
+  justify-content: space-between;
+
+  padding: 8px min(46px, max(10vw, 10px));
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const iconCSS = css`
+  width: 31px;
+  height: 31px;
+  cursor: pointer;
+
+  fill: var(--gray);
+
+  &:hover,
+  &.active {
+    fill: var(--twitter);
+  }
+`;
+
+export const HomeIcon = styled(Home)`
+  ${iconCSS}
+`;
+export const SearchIcon = styled(Search)`
+  ${iconCSS}
+`;
+export const BellIcon = styled(Notifications)`
+  ${iconCSS}
+`;
+export const EmailIcon = styled(Email)`
+  ${iconCSS}
+`;
