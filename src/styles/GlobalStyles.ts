58a9ab36dfd529d04551649124877493189680d1
mateusamarall <mateus.amaral018@gmail.com>
2020-07-20 19:49:57 -0300

    global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,28 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-  *{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-    color:var(--white);
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: var(--white);
   }
-  html,border-style, #root{
-    max-height:100vh;
-    max-width:100vw;
-
-    width:100%;
-    height:100%;
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+    width: 100%;
+    height: 100%;
   }
-  *, button, input{
-    border:0;
-    background:none;
-    font-family: -apple-system, system-ui ,BlinkMacSystemFont,'Segoe UI', Roboto,'Helvetica Neue', Ubuntu,arial,sans-serif;
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
   }
-  html{
-    background:var(--primary);
+  html {
+    background: var(--primary);
   }
-  :root{
+  :root {
     --primary: #000;
     --secondary: #15181C;
     --search: #202327;
