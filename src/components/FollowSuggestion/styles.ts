2d3ea71db14205e725eed1e76c573ee1508deb18
mateusamarall <mateus.amaral018@gmail.com>
2020-07-21 20:26:00 -0300

    Component FollowSuggestion

--- src/components/FollowSuggestion/styles.ts
+++ src/components/FollowSuggestion/styles.ts
@@ -6,7 +6,6 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
   > div {
     display: flex;
     align-items: center;
@@ -16,22 +15,23 @@ export const Container = styled.div`
 export const Avatar = styled.div`
   width: 49px;
   height: 49px;
-  background: 50%;
+  background: var(--gray);
+  border-radius: 50%;
   margin-right: 10px;
 `;
+
 export const Info = styled.div`
   display: flex;
   flex-direction: column;
-
   > strong {
     font-size: 14px;
   }
-
   > span {
     font-size: 14px;
     color: var(--gray);
   }
 `;
+
 export const FollowButton = styled(Button)`
   padding: 6px 17px;
 `;
