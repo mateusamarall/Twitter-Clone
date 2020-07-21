093f00eafb136ac7c4fe8897eebef76a07ab88ed
mateusamarall <mateus.amaral018@gmail.com>
2020-07-20 21:34:16 -0300

    added the menubar

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Main from '../Main';
+import MenuBar from '../MenuBar';
+
 import { Container, Wrapper } from './styles';
 
 const layouts: React.FC = () => {
   return (
     <Container>
       <Wrapper>
-        {/*<MenuBar/>*/}
+        <MenuBar />
         <Main />
         {/*<SideBar/>*/}
       </Wrapper>
